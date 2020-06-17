110d9c9028313ca1b66a74e42efa85f8a06ee47d
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-17 15:25:03 -0500

    prepare data for training

--- __main__.py
+++ __main__.py
@@ -1,2 +1,15 @@
-if __name__ == '__main__':
-    print('Hello world')
+import pandas as pd
+
+df = pd.read_csv('data/classified_data.csv')
+df.head()
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+df_predictors = df.drop('TARGET CLASS', axis=1)
+scaler.fit(df_predictors)
+
+scaled_predictors = scaler.transform(df_predictors)
+
+X = pd.DataFrame(scaled_predictors, columns=df.columns[:-1])
+y = df['TARGET CLASS']
